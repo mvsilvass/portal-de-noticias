124f852480fc3a1c94b91ab3a0bab1d3016c94a7
mvsilvass <mvsilvasoaress@gmail.com>
2024-11-22 19:34:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,25 +14,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal de notícias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="./src/styles/styles.css">
 </head>
 <body>
     <header>
-        <div>
-            <!-- Colocar algum ícone ou logo -->
-            <h1>Portal de Notícias</h1>
-        </div>
-
-        <nav>
-            <!-- fazer um dropdown -->
-            <div>
-                <h2>Categorias</h2>
-                <ul>
-                    <li><a href="./pages/esportes.php">Esportes</a></li>
-                    <li><a href="./pages/tecnologia.php">Tecnologia</a></li>
-                    <li><a href="./pages/entretenimento.php">Entretenimento</a></li>
-                </ul>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <!-- Logo e Links à Esquerda -->
+                <a class="navbar-brand" href="#">Portal de Notícias</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <!-- Menu de Links de Navegação à Esquerda -->
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tecnologia</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Esportes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Entretenimento</a>
+                        </li>
+                    </ul>
+                    <!-- Link de Login à Direita -->
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">
+                                <i class="fas fa-sign-in-alt"></i> Login
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <a href="./pages/login.php">Login</a>
         </nav>
     </header>
 
@@ -60,5 +77,6 @@
     <footer>
         <p>&copy; 2024 Portal de Notícias</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
